bb17a9f54f0e65856692617be86fd542cf496ebb
Astrid Sanna <astrid.sanna@gmail.com>
2021-04-05 12:43:29 -0700

    new edits -- WIP

--- OR/LRAPA/LRAPA_collocated_sensors.R
+++ OR/LRAPA/LRAPA_collocated_sensors.R
@@ -24,6 +24,8 @@
 # 1) monitors = 7; sensors = 60
 # 2) 18 sensors pwfsl_closestDistance <= 1000; 
 #    13 18 sensors pwfsl_closestDistance <= 500.
+# 3) reasonable data for Amazon Park, Oakridge 2, Springfield City Hall
+
 
 #---- Set up ----
 # libraries 
@@ -306,7 +308,7 @@ summary(CG_lm_ex)
 # Adjusted R-squared:  0.7763 
 
 
-# Elgin Ave Outside  115b76a71be4c69d_10608 ----
+# Elgin Ave Outside ----
 Elgin_Ave <- pat_load(
   id = "115b76a71be4c69d_10608", 
   startdate = startdate, 
@@ -360,9 +362,6 @@ summary(FG_lm_ex)
 # (Intercept)        8.66971    1.36421   6.355  0.00314 **
 # both_data$pa_pm25 -0.02843    0.01013  -2.806  0.04853 * 
 # Adjusted R-squared:  0.5789 
-
-
-
 # Madison MS ----
 Madison  <- pat_load(
   id = "41e41572d3d6161b_3413", 
@@ -389,5 +388,335 @@ summary(Ma_lm_ex)
 # (Intercept)       7.283138   0.296977   24.52   <2e-16 ***
 # both_data$pa_pm25 0.203559   0.008988   22.65   <2e-16 ***
 # Adjusted R-squared:  0.2395
+
+
+
+# Oakridge 1	 ----
+Oakridge1 <- pat_load(
+  id = "fd5f069cc7447626_56793", 
+  startdate = startdate, 
+  enddate = enddate, 
+  timezone = timezone)
+
+pat_multiplot(Oakridge1)
+pat_scatterPlotMatrix(Oakridge1)
+#h:t = -0.850
+# smokiest period: Sep-Oct
+Oa1_lm <- pat_internalFit(Oakridge1,   showPlot = FALSE)
+summary(Oa1_lm)
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 386.07381    4.35050   88.74   <2e-16 ***
+# data$pm25_B  -1.38365    0.04509  -30.68   <2e-16 ***
+# Adjusted R-squared:  0.01514 
+Oa1_lm_ex <- pat_externalFit(Oakridge1, showPlot = FALSE)
+summary(Oa1_lm_ex)
+# try remove august
+Oakridge1 %>% 
+  pat_filterDate(20200901, 20201101) %>%
+  pat_multiplot()
+Oakridge1 %>% 
+  pat_filterDate(20200901, 20201101) %>%
+  pat_scatterPlotMatrix()
+# bad data 
+
+# Oakridge 2	*********************************** ----
+Oakridge2 <- pat_load(
+  id = "f2ace631a501333b_38681", 
+  startdate = startdate, 
+  enddate = enddate, 
+  timezone = timezone)
+
+pat_multiplot(Oakridge2)
+pat_scatterPlotMatrix(Oakridge2)
+#h:t = -0.880
+# smokiest period: Sep-Oct
+Oa2_lm <- pat_internalFit(Oakridge2,   showPlot = FALSE)
+summary(Oa2_lm)
+# Coefficients:
+#               Estimate Std. Error  t value Pr(>|t|)    
+# (Intercept) 4.840e-01  7.971e-03    60.72   <2e-16 ***
+# data$pm25_B 9.666e-01  8.447e-05 11442.67   <2e-16 ***
+# Adjusted R-squared:  0.9994 
+Oa2_lm_ex <- pat_externalFit(Oakridge2, showPlot = FALSE)
+summary(Oa2_lm_ex)
+# Coefficients:
+#                     Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)       -1.514244   0.178614  -8.478   <2e-16 ***
+# both_data$pa_pm25  0.824115   0.002476 332.855   <2e-16 ***
+# Adjusted R-squared:  0.9752 
+
+
+
+# Oakridge 3 (startdate = 20200815) ----
+Oakridge3 <- pat_load(
+  id = "799da69adac45e75_38631", 
+  startdate = startdate, 
+  enddate = enddate, 
+  timezone = timezone)
+
+pat_multiplot(Oakridge3)
+# try remove july till 14 august 
+Oakridge3 %>% pat_filterDate(20200815, 20201101)%>%
+  pat_scatterPlotMatrix()
+#h:t = -0.844
+
+# create new pat without bad data 
+Oakridge3 <- pat_load(
+  id = "799da69adac45e75_38631", 
+  startdate = 20200815, 
+  enddate = enddate, 
+  timezone = timezone)
+
+# smokiest period: Sep-Oct
+Oa3_lm <- pat_internalFit(Oakridge3,   showPlot = TRUE)
+summary(Oa3_lm)
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) -0.4246264  0.0125038  -33.96   <2e-16 ***
+# data$pm25_B  1.1337854  0.0001136 9978.65   <2e-16 ***
+# Adjusted R-squared:  0.9995 
+Oa3_lm_ex <- pat_externalFit(Oakridge3, showPlot = TRUE)
+summary(Oa3_lm_ex)
+# Coefficients:
+#                   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)       -1.393017   0.159202   -8.75   <2e-16 ***
+# both_data$pa_pm25  0.769792   0.005134  149.94   <2e-16 ***
+# Adjusted R-squared:  0.9329 
+
+# Springfield City Hall ********************** -----
+Springfield <- pat_load(
+  id = "12426967f1fc742c_10606", 
+  startdate = startdate, 
+  enddate = enddate, 
+  timezone = timezone)
+
+pat_multiplot(Springfield)
+pat_scatterPlotMatrix(Springfield)
+#h:t = -0.738
+# smokiest period: Sep-Oct
+SC_lm <- pat_internalFit(Springfield,   showPlot = FALSE)
+summary(SC_lm)
+# Coefficients:
+#              Estimate Std. Error  t value Pr(>|t|)    
+# (Intercept) 0.0013514  0.0138823    0.097    0.922    
+# data$pm25_B 0.9827831  0.0001008 9750.023   <2e-16 ***
+# Adjusted R-squared:  0.9992 
+SC_lm_ex <- pat_externalFit(Springfield, showPlot = FALSE)
+summary(SC_lm_ex)
+# Coefficients:
+#                 Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)       1.424547   0.495640   2.874  0.00408 ** 
+# both_data$pa_pm25 0.661990   0.004622 143.238  < 2e-16 ***
+# Adjusted R-squared: 0.8783
+
+#----- create airsensor objects ----
+# Prepare sensor info 
+# Set up months
+timezone <- "America/Los_Angeles"
+monthStamps <- c(202007, 202008, 202009, 202010)
+
+for ( monthStamp in monthStamps ) {
+  
+  logger.debug("Working on monthStamp %s ---------- ...", monthStamp) 
+  
+  # Get POSXct startdate
+  startdate <- MazamaCoreUtils::parseDatetime(monthStamp, timezone = timezone)
+  
+  # Guarantee that the enddate is the first of the next month
+  enddate <- lubridate::floor_date(
+    startdate + lubridate::ddays(40),
+    unit = "month"
+  )
+  
+  # Get YYYY and MM strings
+  YYYY <- strftime(startdate, "%Y")
+  MM <- strftime(startdate, "%m")
+  
+  # Initialize counters
+  idCount <- length(near_sensorsID)
+  count <- 0 
+  successCount <- 0
+# Create the archiveDir/airsensor/YYYY/ directory
+dir.create(
+  file.path(archiveDir, "airsensor", YYYY), 
+  showWarnings = FALSE,
+  recursive = TRUE
+)
+
+# Assign a collection name that makes sense
+collectionName <- "lrapa"
+
+# Init counts
+successCount <- 0
+count <- 0
+
+dataList <- list()
+
+# Loop over all ids and aggregate to hourly
+for ( id in near_sensorsID ) {
+  
+  count <- count + 1
+  
+  # Debug info
+  logger.debug(
+    "%4d/%d Calling pat_createAirSensor('%s')",
+    count,
+    length(near_sensorsID),
+    id
+  )
+  
+  # Load the pat data, convert to an airsensor and add to dataList
+  dataList[[id]] <- tryCatch(
+    expr = {
+      airsensor <- pat_load(
+        id = id,
+        label = NULL,
+        pas = LRAPA_sensors,
+        startdate = startdate,
+        enddate = enddate,
+        timezone = "America/Los_Angeles"
+      ) %>%
+        pat_createAirSensor(
+          FUN = AirSensor::PurpleAirQC_hourly_AB_00
+        )
+    }, 
+    error = function(e) {
+      logger.warn('Unable to load PAT data for %s ', id)
+      NULL
+    }
+    
+    # Keep going in the face of errors
+  )
+  
+} # END of deviceDeploymentIDs loop
+
+# Combine the airsensors into a single airsensor object and save
+tryCatch(
+  expr = {
+    logger.info('Combining airsensors...')
+    
+    airsensor <- PWFSLSmoke::monitor_combine(dataList)
+    class(airsensor) <- c("airsensor", "ws_monitor", "list")
+    
+    logger.info('Combined successfully...')
+    
+    # Create Airsensor canonical file name
+    fileName <- paste0("airsensor_", collectionName, "_", YYYY, MM, ".rda")
+    
+    # Create Airsensor canonical file path
+    filePath <- file.path(archiveDir, "airsensor", YYYY, fileName)
+    
+    save(list = "airsensor", file = filePath)
+  }, 
+  error = function(e) {
+    msg <- paste("Error creating monthly AirSensor file: ", e)
+    logger.error(msg)
+  }
+)
+
+# Now proceed to the next month
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----- airsensor analysis ----
+All_sensors <- sensor_load(
+  collection = "lrapa",
+  startdate = startdate,
+  enddate = enddate,
+  timezone = timezone
+)
+monitor_timeseriesPlot(All_sensor)
+All_sensors_meta <- All_sensors$meta
+
+# Amazon Park sensor 
+AP_sensor <- monitor_subset(
+  All_sensors,
+  monitorIDs = 	"947c72aa269258cc_56971",
+  dropMonitors = TRUE,
+  timezone = timezone
+)
+monitor_timeseriesPlot(AP_sensor)
+
+AP_sensor %>% sensor_calendarPlot()
+
+# Oakridge 2 
+Oa_sensor <- monitor_subset(
+  All_sensors,
+  monitorIDs = 	"f2ace631a501333b_38681",
+  dropMonitors = TRUE,
+  timezone = timezone
+)
+monitor_timeseriesPlot(Oa_sensor)
+
+Oa_sensor %>% sensor_calendarPlot()
+
+# Springfield City Hall 
+SC_sensor <- monitor_subset(
+  All_sensors,
+  monitorIDs = 	"12426967f1fc742c_10606",
+  dropMonitors = TRUE,
+  timezone = timezone
+)
+monitor_timeseriesPlot(SC_sensor)
+SC_sensor %>% sensor_calendarPlot()
+
+# left here: I guess I'll have to create separate sensor objects, one per parameter.
+# extract data 
+AP_sensor_d <- AP_sensor %>% 
+  sensor_extractData()
+  
+test <- pat_createAirSensor(
+  pat = Amazon_Park,
+  parameter = c("pm25","humidity"),
+  FUN = PurpleAirQC_hourly_AB_00)
+
+# Error: Can't subset columns that don't exist.
+# x Column `humidity` doesn't exist.
+# Run `rlang::last_error()` to see where the error occurred.
+# In addition: Warning message:
+# In if (!parameter %in% names(hourlyData)) { :
+#   the condition has length > 1 and only the first element will be used
+
+test_meta <- Amazon_Park$meta
+test_data <- Amazon_Park$data
+
+names(Amazon_Park$data)
 
 
