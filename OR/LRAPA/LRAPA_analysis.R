1706b8a5d659fb9f7ced6bda61149945832928f6
Astrid Sanna <astrid.sanna@gmail.com>
2021-04-08 10:23:07 -0700

    new edits -- WIP

--- OR/LRAPA/LRAPA_analysis.R
+++ OR/LRAPA/LRAPA_analysis.R
@@ -1,13 +1,13 @@
 ################################################################
 # Author: Astrid Sanna
 # Issue: LRAPA colocated sensors #1 (MazamaScience/reports)
-# Date: 4/7/2021
+# Date: 4/8/2021
 ################################################################
 # Now that we have found colocate sensors (within 100 m from the closest FRM 
 # monitor) we can start exploring the pat files by running pat_scatterPlotMatrix()
 # pat_internalFit and pat_externalFit, and select the best performing sensors.
 
-# ----- Create a table including only info useful for your analysis ------------
+# ----- Create a table including only $meta useful to your analysis ------------
 near_sensors_tb <- near_sensors %>%
   dplyr::select(label, 
          deviceDeploymentID, 
@@ -38,16 +38,11 @@ summary(AM_lm)
 AM_lm_ex <- pat_externalFit(Amazon_Park, showPlot = TRUE)
 summary(AM_lm_ex)
 
-
-# * other sensor -----
-# * other sensor -----
-
 # ----- Linear moled PM2.5 monitor ~ PM2.5 sensor for specific weeks
 # * Load monitors 
 LRAPA_monitors <- get(load(file.path(archiveDir, "LRAPA_monitors.rda")))
 
-# * Extract PM2.5 hourly data (HD) for the Amazon Park MONITOR ----- 
-
+# * July: PM2.5 hourly data (HD) for the Amazon Park MONITOR
 AP_monitor_HD <-
   LRAPA_monitors %>% 
   monitor_subset(monitorIDs = "410390060_01", tlim=c(20200710, 20200718)) %>% 
@@ -56,7 +51,7 @@ AP_monitor_HD <-
 
 names(AP_monitor_HD) # double check columns names 
 
-# * Extract PM2.5 HD for the Amazon Park SENSOR -----
+# * July: PM2.5 HD for the Amazon Park SENSOR -----
 AP_sensor_HD <-
   Amazon_Park %>%
   pat_filterDate(20200710, 20200718, timezone = timezone) %>% 
@@ -67,4 +62,47 @@ AP_sensor_HD <-
 
 names(AP_sensor_HD) # double check columns names 
 
-# run lm analysis -- left here 
+# * combine monitor and sensor data
+AP_comb_07 <- dplyr::left_join(AP_sensor_HD, AP_monitor_HD, by = "datetime")
+names(AP_comb_07) #check columns 
+# note: all columns come from the sensor's pat object exept for "monitor_pm25"
+
+# * run lm 
+AP_lm <- lm(AP_comb_07$monitor_pm25 ~ AP_comb_07$pm25)
+summary(AP_lm)
+plot(AP_comb_07$monitor_pm25 ~ AP_comb_07$pm25, col=1, 
+     pch=16, main  = "Amazon Park -- July 10-18, 2020", 
+     xlab = "Sensor PM2.5 hourly data", 
+     ylab = "Monitor PM2.5 hourly data")
+abline(AP_lm, col = "red")
+# Coefficients:
+#                 Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)      3.32711    0.06316   52.68   <2e-16 ***
+# AP_comb_07$pm25  0.38453    0.01105   34.81   <2e-16 ***
+
+# Adjusted R-squared:  0.8675 
+
+# * run multiple lm w/ humidity 
+AP_mlm <- lm(AP_comb_07$monitor_pm25 ~ AP_comb_07$pm25 + AP_comb_07$humidity)
+summary(AP_mlm)
+# Coefficients:
+#                       Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)          3.981280   0.076219   52.23   <2e-16 ***
+# AP_comb_07$pm25      0.422325   0.009184   45.98   <2e-16 ***
+# AP_comb_07$humidity -0.018184   0.001627  -11.18   <2e-16 ***
+
+# Adjusted R-squared:  0.9208 
+
+plot(AP_comb_07$monitor_pm25 ~ AP_comb_07$pm25 + AP_comb_07$humidity, col=1, 
+     pch=16, main  = "Amazon Park -- July 10-18, 2020", 
+     xlab = "Sensor hourly data", 
+     ylab = "Monitor PM2.5 hourly data")
+
+
+
+
+
+
+
+
+
