1ecce3d647d2fe93b7534880a664d0eaa991f852
Astrid Sanna <astrid.sanna@gmail.com>
2021-04-12 13:03:02 -0700

    new edits -- WIP

--- OR/LRAPA/LRAPA_Amazon_Park.R
+++ OR/LRAPA/LRAPA_Amazon_Park.R
@@ -1,7 +1,7 @@
 ################################################################
 # Author: Astrid Sanna
 # Issue: LRAPA colocated sensors #1 (MazamaScience/reports)
-# Date: 4/9/2021
+# Date: 4/12/2021
 ################################################################
 # Now that we have found colocate sensors (within 100 m from the closest FRM 
 # monitor) we can start exploring the pat files by running pat_scatterPlotMatrix()
@@ -11,7 +11,7 @@
 # hourly data (PM2.5).
 # 2) fit linear and multilinear models to explore the influence of humidity on the 
 # sensor performance.
-# 3) compare results from step 2) to linearly modeled predicted results.   
+# 3) compare results from step 2) to linearly modeled predicted data.   
 
 
 # ----- Setup --------------------------------------------------------------
@@ -19,6 +19,9 @@
 library(MazamaCoreUtils)   
 library(AirSensor)
 library(PWFSLSmoke)
+library(fBasics)
+library(car)
+library(MASS)
 
 # * archiveDir -----
 archiveDir <- "C:/Users/astri/Mirror/Mazamascience/Projects/Data/LRAPA"
@@ -67,13 +70,14 @@ summary(AM_lm_ex)
 # 2) "Source" the script 
 # 3) Check that you have sensorMonitorFit() in your R environment 
 
-# * July data frame: sensor and monitor hourly data
+# * July 01-08 data frame: sensor and monitor hourly data
+# low PM25 week according to pat_internalFit() plot  
 AP_df_07<- sensorMonitorFit(
   pat = Amazon_Park,
   ws_monitor = LRAPA_monitors,
   monitorID = "410390060_01",
-  startdate = 20200710,
-  enddate = 20200718)
+  startdate = 20200701,
+  enddate = 20200708)
 View(AP_df_07)
 # note: all columns come from the sensor's pat object exept for "monitor_pm25"
 
@@ -83,19 +87,53 @@ AP_lm <- lm(AP_df_07$pm25_monitor ~ AP_df_07$pm25)
 # check lm results
 summary(AP_lm)
 
-# create a lm results list
-lm07_results <- list(summary(AP_lm)$adj.r.squared,
-                      summary(AP_lm)$coefficients[1],
-                      summary(AP_lm)$coefficients[2],
-                      summary(AP_lm)$coefficients[3])
+# create a lm results df
+lm07_results <- data.frame(
+  round(summary(AP_lm)$adj.r.squared,3),
+  round(summary(AP_lm)$coefficients[1],3),
+  round(summary(AP_lm)$coefficients[2],3),
+  round(summary(AP_lm)$coefficients[3],3))
+
 View(lm07_results)
 
-plot(AP_df_07$monitor_pm25 ~ AP_df_07$pm25, col=1, 
-     pch=16, main  = "Amazon Park -- July 10-18, 2020", 
+lm07_results <- lm07_results %>%
+  rename(
+    R_sq = paste(names(lm07_results[1])),
+    intercept = paste(names(lm07_results[2])),
+    PM25 = paste(names(lm07_results[3])),
+    Humidity = paste(names(lm07_results[4])))
+
+View(lm07_results)
+
+# predict for July 
+
+# subset df by extracting sensor pm25 and humidity variables 
+pred_data07 <- AP_df_07 %>%
+  dplyr::select(pm25, humidity)
+
+# pedict response (monitor pm25)
+pred_data07$pred_fit <- predict(AP_lm, newdata= pred_data07)
+View(pred_data07)
+
+# plot lm 
+par(mfrow=c(1,1))
+plot(AP_df_07$pm25_monitor ~ AP_df_07$pm25, col=1, 
+     pch=16, main  = "Amazon Park -- July 1-8, 2020", 
      xlab = "Sensor PM2.5 hourly data", 
      ylab = "Monitor PM2.5 hourly data")
 abline(AP_lm, col = "red")
+lines(pred_data07$pred_fit ~ pred_data07$pm25, col = "blue")
 
+
+# compare to lm predicted values
+lm_predicted <- lm(pred_data07$pred_fit ~ pred_data07$pm25)
+summary(lm_predicted)
+
+plot(pred_data07$pred_fit ~ AP_df_07$pm25, col=1, 
+     pch=16, main  = "Amazon Park -- July 1-8, 2020")
+
+?predict
+?abline
 
 # * run multiple lm w/ humidity -----
 AP_mlm <- lm(AP_df_07$pm25_monitor ~ AP_df_07$pm25 + AP_df_07$humidity)
@@ -103,48 +141,67 @@ AP_mlm <- lm(AP_df_07$pm25_monitor ~ AP_df_07$pm25 + AP_df_07$humidity)
 # check mlm results
 summary(AP_mlm)
 
-# create a mlm results list
-mlm07_results <- list(summary(AP_mlm)$adj.r.squared,
-                      summary(AP_mlm)$coefficients[1],
-                      summary(AP_mlm)$coefficients[2],
-                      summary(AP_mlm)$coefficients[3])
+# create a mlm results df
+mlm07_results <- data.frame(
+  round(summary(AP_mlm)$adj.r.squared,3),
+  round(summary(AP_mlm)$coefficients[1],3),
+  round(summary(AP_mlm)$coefficients[2],3),
+  round(summary(AP_mlm)$coefficients[3],3))
+
 View(mlm07_results)
 
-plot(AP_df_07$monitor_pm25 ~ AP_df_07$pm25 + AP_df_07$humidity, col=1, 
-     pch=16, main  = "Amazon Park -- July 10-18, 2020", 
-     xlab = "Sensor hourly data", 
+mlm07_results <- mlm07_results %>%
+  rename(
+    R_sq = paste(names(mlm07_results[1])),
+    intercept = paste(names(mlm07_results[2])),
+    PM25 = paste(names(mlm07_results[3])),
+    Humidity = paste(names(mlm07_results[4])))
+
+View(mlm07_results)
+
+# pedict response (monitor pm25)
+pred_data07$pred_mfit <- predict(AP_mlm, newdata= pred_data07)
+View(pred_data07)
+
+par(mfrow=c(2,2))
+plot(AP_df_07$pm25_monitor ~ AP_df_07$pm25 + AP_df_07$humidity, col=1, 
+     pch=16, main  = "Amazon Park -- July 1-8, 2020", 
      ylab = "Monitor PM2.5 hourly data") 
 
-
 # * September data frame: sensor and monitor hourly data
+# smokiest week according to pat_internalFit() plot 
 AP_df_09 <- sensorMonitorFit(
   pat = Amazon_Park,
   ws_monitor = LRAPA_monitors,
   monitorID = "410390060_01",
-  startdate = 20200710,
-  enddate = 20200718)
+  startdate = 20200905,
+  enddate = 20200913)
 View(AP_df_09)
 # note: all columns come from the sensor's pat object exept for "monitor_pm25"
 
 # * run lm ----
-AP_lm <- lm(AP_df_09$monitor_pm25 ~ AP_df_09$pm25)
+AP_lm <- lm(AP_df_09$pm25_monitor ~ AP_df_09$pm25)
 
 # check lm results
 summary(AP_lm)
 
-# create a lm results list
-lm09_results <- data.frame(summary(AP_lm)$adj.r.squared,
-                       summary(AP_lm)$coefficients[1],
-                       summary(AP_lm)$coefficients[2],
-                       summary(AP_lm)$coefficients[3])
-# lm09_results %>% 
-#   rename(
-#     intercept = summary(AP_lm)$coefficients[1],
-#     PM25 = summary(AP_lm)$coefficients[2],
-#     Humidity = summary(AP_lm)$coefficients[3],
-#     R_sq = summary(AP_lm)$adj.r.squared)
-# 
-# View(lm09_results)
+# create a lm results df
+lm09_results <- data.frame(
+  round(summary(AP_lm)$adj.r.squared,3),
+  round(summary(AP_lm)$coefficients[1],3),
+  round(summary(AP_lm)$coefficients[2],3),
+  round(summary(AP_lm)$coefficients[3],3))
+
+View(lm09_results)
+
+lm09_results <- lm09_results %>%
+  rename(
+    R_sq = paste(names(lm09_results[1])),
+    intercept = paste(names(lm09_results[2])),
+    PM25 = paste(names(lm09_results[3])),
+    Humidity = paste(names(lm09_results[4])))
+   
+View(lm09_results)
 
 # plot lm
 plot(AP_df_09$monitor_pm25 ~ AP_df_09$pm25, col=1, 
@@ -154,24 +211,36 @@ plot(AP_df_09$monitor_pm25 ~ AP_df_09$pm25, col=1,
 abline(AP_lm, col = "red")
 
 # * run multiple lm w/ humidity -----
-AP_mlm <- lm(AP_df_09$monitor_pm25 ~ AP_df_09$pm25 + AP_df_09$humidity)
+AP_mlm <- lm(AP_df_09$pm25_monitor ~ AP_df_09$pm25 + AP_df_09$humidity)
 
 # check mlm results 
 summary(AP_mlm)
 
-# create a mlm results list
-mlm09_results <- list(summary(AP_mlm)$adj.r.squared,
-                     summary(AP_mlm)$coefficients[1],
-                     summary(AP_mlm)$coefficients[2],
-                     summary(AP_mlm)$coefficients[3])
+# create a mlm results df
+mlm09_results <- data.frame(
+  round(summary(AP_mlm)$adj.r.squared,3),
+  round(summary(AP_mlm)$coefficients[1],3),
+  round(summary(AP_mlm)$coefficients[2],3),
+  round(summary(AP_mlm)$coefficients[3],3))
+
+View(mlm09_results)
+
+mlm09_results <- mlm09_results %>%
+  rename(
+    R_sq = paste(names(mlm09_results[1])),
+    intercept = paste(names(mlm09_results[2])),
+    PM25 = paste(names(mlm09_results[3])),
+    Humidity = paste(names(mlm09_results[4])))
+
+View(mlm09_results)
 
 # plot mlm
-plot(AP_df_09$monitor_pm25 ~ AP_df_09$pm25 + AP_df_09$humidity, col=1, 
-     pch=16, main  = "Amazon Park -- Sept 7-14, 2020", 
-     xlab = "Sensor hourly data", 
+par(mfrow=c(2,2))
+plot(AP_df_09$pm25_monitor ~ AP_df_09$pm25 + AP_df_09$humidity, col=1, 
+     pch=16, main  = "Amazon Park -- Sept 5-13, 2020", 
      ylab = "Monitor PM2.5 hourly data") 
 
 
-?predict.lm
+
 
 
